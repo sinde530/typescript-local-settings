24de8b9f9147d28b59f7d8135c427a9db757a40a
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-07 12:12:44 +0900

    redux settons

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+
+import "./index.css";
+
+import App from "./App";
+
+const store = createStore(rootReducer);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
-)
+  </Provider>
+);
